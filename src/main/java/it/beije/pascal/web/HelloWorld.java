642997de1192819cd3a047577c9820c3d5f547fe
ivfly <iv0fly@gmail.com>
2022-01-24 15:47:39 +0100

    Hello HTML

---
+++ src/main/java/it/beije/pascal/web/HelloWorld.java
@@ -0,0 +1,69 @@
+package it.beije.pascal.web;
+
+import java.io.IOException;
+import java.time.LocalTime;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/hello")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorld() {
+        super();
+        System.out.println("hello!!");
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("HelloWorld GET");
+		StringBuilder html = new StringBuilder("<!DOCTYPE html>\r\n" + 
+				"<html>" + 
+				"<head>" + 
+				"<meta charset=\"ISO-8859-1\">" + 
+				"<title>PASCAL HTTP SERVLET</title>" + 
+				"</head>" + 
+				"<body>" + 
+				"<h1>HELLO WORLD</h1>");
+		
+		html.append("Sono le ").append(LocalTime.now().toString()).append("</body></html>");
+		
+		response.getWriter().append(html.toString());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("HelloWorld POST");
+		String fname = request.getParameter("fname");
+		String lname = request.getParameter("lname");
+
+		StringBuilder html = new StringBuilder("<!DOCTYPE html>\r\n" + 
+				"<html>" + 
+				"<head>" + 
+				"<meta charset=\"ISO-8859-1\">" + 
+				"<title>PASCAL HTTP SERVLET</title>" + 
+				"</head>" + 
+				"<body>");
+		
+		html.append("FNAME: ").append(fname)
+			.append("<br/>").append("LNAME: ").append(lname)
+			.append("</body></html>");
+		
+		response.getWriter().append(html.toString());
+	}
+
+}
