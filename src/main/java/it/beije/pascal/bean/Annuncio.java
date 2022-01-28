a53ae6ce1fed908cc3aace84ff5482f861d753b6
Andrea Gliori <andrea.gliori@gmail.com>
2022-01-28 14:38:54 +0100

    + ServletUtility

--- src/main/java/it/beije/pascal/bean/Annuncio.java
+++ src/main/java/it/beije/pascal/bean/Annuncio.java
@@ -11,15 +11,14 @@ import javax.persistence.Table;
 
 import it.beije.pascal.bean.enums.*;
 
-
 @Entity
 @Table(name = "annuncio")
 public class Annuncio {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	
+
 	@Column(name = "venditori_id")
 	private int venditoriId;
 	@Column(name = "annuncio_multiplo_id")
@@ -30,7 +29,7 @@ public class Annuncio {
 	private int indirizzoId;
 	@Column(name = "edificio_id")
 	private int edificioId;
-	
+
 	@Column(name = "tipo_immobile")
 	private TipoImmobile tipoImmobile;
 	@Column(name = "tipo_annuncio")
@@ -68,205 +67,291 @@ public class Annuncio {
 	private boolean virtualTour;
 	@Column(name = "create_timestamp")
 	private Timestamp createTimestamp;
-	
-	
-	
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public int getVenditoriId() {
 		return venditoriId;
 	}
+
 	public void setVenditoriId(int venditoriId) {
 		this.venditoriId = venditoriId;
 	}
+
 	public int getAnnuncio_multiplo_id() {
 		return annuncio_multiplo_id;
 	}
+
 	public void setAnnuncio_multiplo_id(int annuncio_multiplo_id) {
 		this.annuncio_multiplo_id = annuncio_multiplo_id;
 	}
+
 	public int getPiantinaId() {
 		return piantinaId;
 	}
+
 	public void setPiantinaId(int piantinaId) {
 		this.piantinaId = piantinaId;
 	}
+
 	public int getIndirizzoId() {
 		return indirizzoId;
 	}
+
 	public void setIndirizzoId(int indirizzoId) {
 		this.indirizzoId = indirizzoId;
 	}
+
 	public int getEdificioId() {
 		return edificioId;
 	}
+
 	public void setEdificioId(int edificioId) {
 		this.edificioId = edificioId;
 	}
+
 	public TipoImmobile getTipoImmobile() {
 		return tipoImmobile;
 	}
+
 	public void setTipoImmobile(TipoImmobile tipoImmobile) {
 		this.tipoImmobile = tipoImmobile;
 	}
+
 	public TipoAnnuncio getTipoAnnuncio() {
 		return tipoAnnuncio;
 	}
+
 	public void setTipoAnnuncio(TipoAnnuncio tipoAnnuncio) {
 		this.tipoAnnuncio = tipoAnnuncio;
 	}
+
 	public int getPrezzo() {
 		return prezzo;
 	}
+
 	public void setPrezzo(int prezzo) {
 		this.prezzo = prezzo;
 	}
+
 	public int getMq() {
 		return mq;
 	}
+
 	public void setMq(int mq) {
 		this.mq = mq;
 	}
+
 	public int getLocali() {
 		return locali;
 	}
+
 	public void setLocali(int locali) {
 		this.locali = locali;
 	}
+
 	public int getBagni() {
 		return bagni;
 	}
+
 	public void setBagni(int bagni) {
 		this.bagni = bagni;
 	}
+
 	public int getTotPiani() {
 		return totPiani;
 	}
+
 	public void setTotPiani(int totPiani) {
 		this.totPiani = totPiani;
 	}
+
 	public int getPiano() {
 		return piano;
 	}
+
 	public void setPiano(int piano) {
 		this.piano = piano;
 	}
+
 	public boolean isAscensore() {
 		return ascensore;
 	}
+
 	public void setAscensore(boolean ascensore) {
 		this.ascensore = ascensore;
 	}
+
 	public int getPostiAuto() {
 		return postiAuto;
 	}
+
 	public void setPostiAuto(int postiAuto) {
 		this.postiAuto = postiAuto;
 	}
+
 	public boolean isBalcone() {
 		return balcone;
 	}
+
 	public void setBalcone(boolean balcone) {
 		this.balcone = balcone;
 	}
+
 	public boolean isTerrazzo() {
 		return terrazzo;
 	}
+
 	public void setTerrazzo(boolean terrazzo) {
 		this.terrazzo = terrazzo;
 	}
+
 	public Giardino getGiardino() {
 		return giardino;
 	}
+
 	public void setGiardino(Giardino giardino) {
 		this.giardino = giardino;
 	}
+
 	public Condizione getCondizione() {
 		return condizione;
 	}
+
 	public void setCondizione(Condizione condizione) {
 		this.condizione = condizione;
 	}
+
 	public ClasseEnergetica getClasseEnergetica() {
 		return classeEnergetica;
 	}
+
 	public void setClasseEnergetica(ClasseEnergetica classeEnergetica) {
 		this.classeEnergetica = classeEnergetica;
 	}
+
 	public Riscaldamento getRiscaldamento() {
 		return riscaldamento;
 	}
+
 	public void setRiscaldamento(Riscaldamento riscaldamento) {
 		this.riscaldamento = riscaldamento;
 	}
+
 	public AriaCondizionata getAriaCondizionata() {
 		return ariaCondizionata;
 	}
+
 	public void setAriaCondizionata(AriaCondizionata ariaCondizionata) {
 		this.ariaCondizionata = ariaCondizionata;
 	}
+
 	public Arredamento getArredamento() {
 		return arredamento;
 	}
+
 	public void setArredamento(Arredamento arredamento) {
 		this.arredamento = arredamento;
 	}
+
 	public boolean isPiscina() {
 		return piscina;
 	}
+
 	public void setPiscina(boolean piscina) {
 		this.piscina = piscina;
 	}
+
 	public boolean isPortineria() {
 		return portineria;
 	}
+
 	public void setPortineria(boolean portineria) {
 		this.portineria = portineria;
 	}
+
 	public int getAnno_costruzione() {
 		return anno_costruzione;
 	}
+
 	public void setAnno_costruzione(int anno_costruzione) {
 		this.anno_costruzione = anno_costruzione;
 	}
+
 	public StatoRogito getStatoRogito() {
 		return statoRogito;
 	}
+
 	public void setStatoRogito(StatoRogito statoRogito) {
 		this.statoRogito = statoRogito;
 	}
+
 	public boolean isVisitaGuidata() {
 		return visitaGuidata;
 	}
+
 	public void setVisitaGuidata(boolean visitaGuidata) {
 		this.visitaGuidata = visitaGuidata;
 	}
+
 	public String getDescrizioneLunga() {
 		return descrizioneLunga;
 	}
+
 	public void setDescrizioneLunga(String descrizioneLunga) {
 		this.descrizioneLunga = descrizioneLunga;
 	}
+
 	public boolean isVirtualTour() {
 		return virtualTour;
 	}
+
 	public void setVirtualTour(boolean virtualTour) {
 		this.virtualTour = virtualTour;
 	}
+
 	public Timestamp getCreateTimestamp() {
 		return createTimestamp;
 	}
+
 	public void setCreateTimestamp(Timestamp createTimestamp) {
 		this.createTimestamp = createTimestamp;
 	}
-	
 
-	
-	
-	
-	
+	@Override
+	public String toString() {
+		return "Annuncio [id=" + id + ", venditoriId=" + venditoriId + ", annuncio_multiplo_id=" + annuncio_multiplo_id
+				+ ", piantinaId=" + piantinaId + ", indirizzoId=" + indirizzoId + ", edificioId=" + edificioId
+				+ ", tipoImmobile=" + tipoImmobile + ", tipoAnnuncio=" + tipoAnnuncio + ", prezzo=" + prezzo + ", mq="
+				+ mq + ", locali=" + locali + ", bagni=" + bagni + ", totPiani=" + totPiani + ", piano=" + piano
+				+ ", ascensore=" + ascensore + ", postiAuto=" + postiAuto + ", balcone=" + balcone + ", terrazzo="
+				+ terrazzo + ", giardino=" + giardino + ", condizione=" + condizione + ", classeEnergetica="
+				+ classeEnergetica + ", riscaldamento=" + riscaldamento + ", ariaCondizionata=" + ariaCondizionata
+				+ ", arredamento=" + arredamento + ", piscina=" + piscina + ", portineria=" + portineria
+				+ ", anno_costruzione=" + anno_costruzione + ", statoRogito=" + statoRogito + ", visitaGuidata="
+				+ visitaGuidata + ", descrizioneLunga=" + descrizioneLunga + ", virtualTour=" + virtualTour
+				+ ", createTimestamp=" + createTimestamp + ", getId()=" + getId() + ", getVenditoriId()="
+				+ getVenditoriId() + ", getAnnuncio_multiplo_id()=" + getAnnuncio_multiplo_id() + ", getPiantinaId()="
+				+ getPiantinaId() + ", getIndirizzoId()=" + getIndirizzoId() + ", getEdificioId()=" + getEdificioId()
+				+ ", getTipoImmobile()=" + getTipoImmobile() + ", getTipoAnnuncio()=" + getTipoAnnuncio()
+				+ ", getPrezzo()=" + getPrezzo() + ", getMq()=" + getMq() + ", getLocali()=" + getLocali()
+				+ ", getBagni()=" + getBagni() + ", getTotPiani()=" + getTotPiani() + ", getPiano()=" + getPiano()
+				+ ", isAscensore()=" + isAscensore() + ", getPostiAuto()=" + getPostiAuto() + ", isBalcone()="
+				+ isBalcone() + ", isTerrazzo()=" + isTerrazzo() + ", getGiardino()=" + getGiardino()
+				+ ", getCondizione()=" + getCondizione() + ", getClasseEnergetica()=" + getClasseEnergetica()
+				+ ", getRiscaldamento()=" + getRiscaldamento() + ", getAriaCondizionata()=" + getAriaCondizionata()
+				+ ", getArredamento()=" + getArredamento() + ", isPiscina()=" + isPiscina() + ", isPortineria()="
+				+ isPortineria() + ", getAnno_costruzione()=" + getAnno_costruzione() + ", getStatoRogito()="
+				+ getStatoRogito() + ", isVisitaGuidata()=" + isVisitaGuidata() + ", getDescrizioneLunga()="
+				+ getDescrizioneLunga() + ", isVirtualTour()=" + isVirtualTour() + ", getCreateTimestamp()="
+				+ getCreateTimestamp() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()="
+				+ super.toString() + "]";
+	}
+
 }
---
+++ src/main/java/it/beije/pascal/utility/ServletUtility.java
@@ -0,0 +1,124 @@
+package it.beije.pascal.utility;
+
+import javax.servlet.http.HttpServletRequest;
+
+import it.beije.pascal.bean.Annuncio;
+import it.beije.pascal.bean.Indirizzo;
+import it.beije.pascal.bean.enums.AriaCondizionata;
+import it.beije.pascal.bean.enums.Arredamento;
+import it.beije.pascal.bean.enums.ClasseEnergetica;
+import it.beije.pascal.bean.enums.Condizione;
+import it.beije.pascal.bean.enums.Giardino;
+import it.beije.pascal.bean.enums.Riscaldamento;
+import it.beije.pascal.bean.enums.StatoRogito;
+import it.beije.pascal.bean.enums.TipoAnnuncio;
+import it.beije.pascal.bean.enums.TipoImmobile;
+
+public class ServletUtility {
+
+	public static Indirizzo createAddress(HttpServletRequest request) {
+
+		Indirizzo indirizzo = new Indirizzo();
+		indirizzo.setComune(request.getParameter("comune"));
+		indirizzo.setIndirizzo(request.getParameter("indirizzo"));
+		indirizzo.setCap(request.getParameter("cap"));
+		indirizzo.setNCivico(Integer.parseInt(request.getParameter("numeroCivico")));
+
+		return indirizzo;
+
+	}
+
+	public static Annuncio createAdvertisement(HttpServletRequest request) {
+		
+		Annuncio annuncio = new Annuncio();
+		String ti = request.getParameter("tipoImmobile").toUpperCase();
+		TipoImmobile tipoImmobile = TipoImmobile.valueOf(ti);
+
+		String ta = request.getParameter("tipoAnnuncio").toUpperCase();
+		TipoAnnuncio tipoAnnuncio = TipoAnnuncio.valueOf(ta);
+
+		int prezzo = Integer.parseInt(request.getParameter("prezzo"));
+
+		int mq = Integer.parseInt(request.getParameter("mq"));
+
+		int locali = Integer.parseInt(request.getParameter("locali"));
+
+		int bagni = Integer.parseInt(request.getParameter("bagni"));
+
+		int tot_piani = Integer.parseInt(request.getParameter("tot_piani"));
+
+		int piano = Integer.parseInt(request.getParameter("piano"));
+
+		boolean ascensore = Boolean.parseBoolean(request.getParameter("ascensore"));
+
+		int postiAuto = Integer.parseInt(request.getParameter("postiAuto"));
+
+		boolean balcone = Boolean.parseBoolean(request.getParameter("balcone"));
+
+		boolean terrazzo = Boolean.parseBoolean(request.getParameter("terrazzo"));
+
+		String g = request.getParameter("giardino").toUpperCase();
+		Giardino giardino = Giardino.valueOf(g);
+
+		String c = request.getParameter("condizione").toUpperCase();
+		Condizione condizione = Condizione.valueOf(c);
+
+		String ce = request.getParameter("classeEnergetica").toUpperCase();
+		ClasseEnergetica classeEnergetica = ClasseEnergetica.valueOf(ce);
+
+		String r = request.getParameter("riscaldamento").toUpperCase();
+		Riscaldamento riscaldamento = Riscaldamento.valueOf(r);
+
+		String ac = request.getParameter("ariaCondizionata").toUpperCase();
+		AriaCondizionata ariaCondizionata = AriaCondizionata.valueOf(ac);
+
+		String ar = request.getParameter("arredamento").toUpperCase();
+		Arredamento arredamento = Arredamento.valueOf(ar);
+
+		boolean piscina = Boolean.parseBoolean(request.getParameter("piscina"));
+
+		boolean portineria = Boolean.parseBoolean(request.getParameter("portineria"));
+
+		short annoCostruzione = Short.parseShort(request.getParameter("annoCostruzione"));
+
+		String sr = request.getParameter("statoRogito").toUpperCase();
+		StatoRogito statoRogito = StatoRogito.valueOf(sr);
+
+		boolean visitaGuidata = Boolean.parseBoolean(request.getParameter("visitaGuidata"));
+
+		String descrizioneLunga = request.getParameter("descrizioneLunga");
+
+		boolean virtualTour = Boolean.parseBoolean(request.getParameter("virtualTour"));
+
+		
+		annuncio.setTipoImmobile(tipoImmobile);
+		annuncio.setTipoAnnuncio(tipoAnnuncio);
+		annuncio.setPrezzo(prezzo);
+		annuncio.setMq(mq);
+		annuncio.setLocali(locali);
+		annuncio.setBagni(bagni);
+		annuncio.setTotPiani(tot_piani);
+		annuncio.setPiano(piano);
+		annuncio.setAscensore(ascensore);
+		annuncio.setPostiAuto(postiAuto);
+		annuncio.setBalcone(balcone);
+		annuncio.setTerrazzo(terrazzo);
+		annuncio.setGiardino(giardino);
+		annuncio.setCondizione(condizione);
+		annuncio.setClasseEnergetica(classeEnergetica);
+		annuncio.setRiscaldamento(riscaldamento);
+		annuncio.setAriaCondizionata(ariaCondizionata);
+		annuncio.setArredamento(arredamento);
+		annuncio.setPiscina(piscina);
+		annuncio.setPortineria(portineria);
+		annuncio.setAnno_costruzione(annoCostruzione);
+		annuncio.setStatoRogito(statoRogito);
+		annuncio.setVisitaGuidata(visitaGuidata);
+		annuncio.setDescrizioneLunga(descrizioneLunga);
+		annuncio.setVirtualTour(virtualTour);
+		
+		return annuncio;
+
+	}
+
+}
