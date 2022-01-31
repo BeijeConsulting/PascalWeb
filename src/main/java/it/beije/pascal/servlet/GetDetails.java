14e7a51346af78b4001bcf03a6025ffb7a810a02
Rosario <rosario.grossi98@gmail.com>
2022-01-31 10:24:59 +0100

    + Servlet get details Ads

---
+++ src/main/java/it/beije/pascal/servlet/GetDetails.java
@@ -0,0 +1,45 @@
+package it.beije.pascal.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import it.beije.pascal.bean.Annuncio;
+import it.beije.pascal.database.ManagerJPA;
+
+/**
+ * Servlet implementation class GetDetails
+ */
+@WebServlet("/getdetails")
+public class GetDetails extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GetDetails() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int id = Integer.parseInt(request.getParameter("annuncio"));
+		System.out.println(id);
+		Annuncio ann = ManagerJPA.getDetails(id);
+	}
+
+}
